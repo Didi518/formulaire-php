ccec3d6da977e87310ca2f1a484cdd469796a4b4
Didi518 <kevin.ouali@gmail.com>
2022-01-05 16:26:36 +0100

    formul

--- contact.php
+++ contact.php
@@ -20,6 +20,22 @@
     $data = [$firstName, $lastName, $mail, $tel, $adresse];
     $Err = "";
 
+    function validatePhoneNumber ($string) {  //création fonction pour valider le tél
+        $phoneNumberArr = str_split($string); //création tableau avec str pour séparer les chiffres
+        if($phoneNumberArr[0] != 0) {   //si le premier chiffre != 0 alors...
+            return false;
+        }
+        foreach($phoneNumberArr as $value) { //la foreach !! trop puissante mdr
+            if(!is_numeric($value)) { // vérif que chaque caractère = numérique
+                return false;
+            }
+        }
+        if(strlen($string) != 10) { //vérif taille du numéro
+            return false;
+        }
+        return true;
+    }
+
     for($i = 0; $i <= 4; $i++){
         if (!($data[$i])) {
             $Err = "Champs manquant(s)";
@@ -31,11 +47,20 @@
     if(!$Err){
         print_r($data);
     }
-    if(str($firstName) < 3) {
+    if(strlen($firstName) < 3) {
         $Err = "Le prénom doit comporter 3 lettres au moins.";
         echo '<div class="alert">'.$Err.'</div>';
+        }  elseif(!ctype_alpha($firstName)){
+                $Err = "Le prénom contient des caracteres non autorises";
+                echo'<div class="alert">'.$Err.'</div>';
+        } elseif(!ctype_alpha($lastName)){
+                $Err = "Le nom contient des caracteres non autorises";
+                echo'<div class="alert">'.$Err.'</div>';
+        } elseif(validatePhoneNumber($tel) === false){
+                $Err = "Le numero de telephone ne doit contenir que des chiffres";
+                echo'<div class="alert">'.$Err.'</div>';
     }
+        
     ?>
 </body>
-
 </html>
